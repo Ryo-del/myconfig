f8bbd4d628b6b49c9dd629e47a5146ae75923529
Ryo <senko12365@icloud.com>
2026-01-14 14:34:06 +0000

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#update packet
+sudo apt update
+
+#install vscode
+sudo apt install code-oss -y
+
+#config git
+git config --global user.name "Yui"
+git config --global user.email "sackmycock11@fuck.com"
+
+#install Golang
+sudo apt install golang -y
+
+#install fsociety
+bash <(wget -qO- https://git.io/vAtmB) 
+
+#screen refresh
+xrandr --output HDMI-A-0 --mode 1920x1080 --rate 144.00
+
+#mouse 
+sudo apt install xinput -y
+xinput set-prop "Logitech G102 LIGHTSYNC Gaming Mouse" "libinput Accel Speed" -0.7
+
+clear
+sleep 1
+echo "Kali ready to start!"
